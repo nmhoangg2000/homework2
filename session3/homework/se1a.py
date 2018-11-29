8d34b232086fbe23772f5f289dd8ff4726c04d6f
nmhoangg2000 <45127849+nmhoangg2000@users.noreply.github.com>
2018-11-29 11:14:00 +0700

    Update se1a.py

--- session3/homework/se1a.py
+++ session3/homework/se1a.py
@@ -1,5 +1,9 @@
-while True:
-    for i in range(15):
-        i = 2*i + 1
-        print(i**2)
+odd = 0
+while (odd == 0):
+    n = odd*2 + 1
+    print(n**2)
     break
+while (odd < 15):
+    odd += 1
+    n = odd*2 + 1
+    print(n**2)
