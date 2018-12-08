cbb755076c287b988412754638f8bff51e4700b3
nmhoangg2000 <45127849+nmhoangg2000@users.noreply.github.com>
2018-12-08 13:26:25 +0700

    Update study.py

--- session5/homework/study.py
+++ session5/homework/study.py
@@ -1,21 +1,6 @@
 def count_letter_freq(inputStr):
     letter = {
-        "a": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "l": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "w": 0,
+ 
     }
     for i in inputStr:
         letter[i] = inputStr.count(i)
